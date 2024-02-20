6c8aa7201a07a7f58c28b134d996c8cd581d2079
LZDragon <lazydragon193@gmail.com>
2024-02-20 17:45:32 -0500

    Created pet class

--- Assets/Pet.cs
+++ Assets/Pet.cs
@@ -2,17 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pet : MonoBehaviour
+public class Pet
 {
-    // Start is called before the first frame update
-    void Start()
+    private string name;
+    private float fullness;
+    private float happiness;
+    private float energyLevel;
+
+    public Pet(string name)
     {
-        
+        this.name = name;
+        fullness = 100.0f;
+        happiness = 100.0f;
+        energyLevel = 100.0f;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public float EnergyLevel
     {
-        
+        get => energyLevel;
+        set => energyLevel = value;
+    }
+
+    public float Happiness
+    {
+        get => happiness;
+        set => happiness = value;
+    }
+
+    public float Fullness
+    {
+        get => fullness;
+        set => fullness = value;
+    }
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public void Eat(float amount)
+    {
+        if (fullness + amount < 100.0f && fullness + amount > 0.0f)
+        {
+            fullness += amount;
+        }
+        else if(fullness + amount > 100.0f)
+        {
+            fullness = 100.0f;
+        }
+        else
+        {
+            fullness = 0.0f;
+        }
+    }
+
+    public void Rest(float amount)
+    {
+        if (energyLevel + amount < 100.0f && energyLevel + amount > 0.0f)
+        {
+            energyLevel += amount;
+        }
+        else if(energyLevel + amount > 100.0f)
+        {
+            energyLevel = 100.0f;
+        }
+        else
+        {
+            energyLevel = 0.0f;
+        }
+    }
+
+    public void Play(float amount)
+    {
+        if (happiness + amount < 100.0f && happiness + amount > 0.0f)
+        {
+            happiness += amount;
+        }
+        else if(happiness + amount > 100.0f)
+        {
+            happiness = 100.0f;
+        }
+        else
+        {
+            happiness = 0.0f;
+        }
     }
 }
